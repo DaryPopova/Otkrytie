ae3252864abc7c2fffee0e567f5cbe26afdc6749
Degus <pd2405@yandex.ru>
2019-04-10 18:49:29 +0300

    некоторые исправления

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,31 +1,32 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import java.net.URL;
+import static io.restassured.RestAssured.get;
 
 public class ApiTest {
 
     String host = "https://api.github.com/users/";
     ObjectMapper mapper = new ObjectMapper();
     String login = "DaryPopova";
+    Response response = get(host + login);
 
     @BeforeSuite
     public void doRequest() throws Exception {
-        RequestSender.sendGetRequest(host + login);
     }
 
     @Test(description = "GET")
     public void testGithubGetUser() throws Exception {
-        JsonNode tree = mapper.readTree(new URL("https://api.github.com/users/" + login));
+        JsonNode tree = mapper.readTree(response.asString());
         Assert.assertEquals(tree.get("login").asText(), login);
     }
 
     @Test(description = "GET")
     public void testGithubGetLocation() throws Exception {
-        JsonNode tree = mapper.readTree(new URL("https://api.github.com/users/" + login));
-        Assert.assertFalse(tree.has("locat"));
+        JsonNode tree = mapper.readTree(response.asString());
+        Assert.assertFalse(tree.get("company").isNull());
     }
 }
